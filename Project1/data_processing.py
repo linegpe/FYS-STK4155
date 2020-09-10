3ecdd3d901501235cfc3904714a9271ad0348cbb
linegpe <line.gaard.pedersen@gmail.com>
2020-09-10 17:13:57 +0200

    AddNoise function

--- Project1/data_processing.py
+++ Project1/data_processing.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-
 def FrankeFunction(x,y):
 	term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
 	term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -9,21 +8,22 @@ def FrankeFunction(x,y):
 	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
 	return term1 + term2 + term3 + term4
 
-
-# Making a default Design Matrix
 def DesignMatrix(x, y, PolyDeg):
 	x = np.ravel(x)
 	y = np.ravel(y)
-
 	NumElem = int((PolyDeg+2)*(PolyDeg+1)/2)
 	X = np.ones((len(x),NumElem))
-
 	for i in range(1,PolyDeg+1):
 		j = int((i)*(i+1)/2)
 		for k in range(i+1):
 			X[:,j+k] = x**(i-k)*y**k
 	return(X)
 
+def AddNoise(z,seed,alpha,N):
+	np.random.seed(seed)
+	noise = alpha*np.random.randn(N, N)
+	z_with_noise = z+noise
+	return z_with_noise
 
 def DataScaling(x,y,z):
 	# x and y must be raveled
