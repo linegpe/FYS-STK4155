c4b3090fde586494796a36bde91aad5f03d514dc
linegpe <line.gaard.pedersen@gmail.com>
2020-09-16 14:18:06 +0200

    Added cross validation

---
+++ Project1/working/Working_programs/data_processing.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def FrankeFunction(x,y):
+	term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+	term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+	term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+	return term1 + term2 + term3 + term4
+
+def DesignMatrix(x, y, PolyDeg):
+	x = np.ravel(x)
+	y = np.ravel(y)
+	NumElem = int((PolyDeg+2)*(PolyDeg+1)/2)
+	X = np.ones((len(x),NumElem))
+	for i in range(1,PolyDeg+1):
+		j = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,j+k] = x**(i-k)*y**k
+	return(X)
+
+def AddNoise(z,seed,alpha,N):
+	np.random.seed(seed)
+	noise = alpha*np.random.randn(N, N)
+	z_with_noise = z+noise
+	return z_with_noise
+
+def DataScaling(x,y,z):
+	# x and y must be raveled
+	dataset = np.stack((x, y, z)).T
+	scaler = StandardScaler()
+	scaler.fit(dataset)
+	[x_scaled, y_scaled, z_scaled] = scaler.transform(dataset).T
+	return x_scaled, y_scaled, z_scaled, scaler
+
+def DataRescaling(x,y,z,scaler,N):
+	dataset_scaled = np.stack((x, y, z))
+	#scaler.fit(dataset_scaled)
+	dataset_rescaled = scaler.inverse_transform(dataset_scaled.T)
+	x_rescaled = np.zeros((N,N))
+	x_rescaled = np.reshape(dataset_rescaled[:,0], (N,N))
+	y_rescaled = np.zeros((N,N))
+	y_rescaled = np.reshape(dataset_rescaled[:,1], (N,N))
+	z_rescaled = np.zeros((N,N))
+	z_rescaled = np.reshape(dataset_rescaled[:,2], (N,N))
+	return x_rescaled, y_rescaled, z_rescaled
---
+++ Project1/working/Working_programs/print_and_plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+from statistical_functions import R2, MSE, ols_svd, BetaVar
+
+def SurfacePlot(x,y,z_original,z_predicted):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    # Plot the surface.
+    surf = ax.plot_surface(x, y, z_original, cmap=cm.ocean,linewidth=0, antialiased=False)
+    surf = ax.plot_surface(x, y, z_predicted, cmap=cm.Pastel1,linewidth=0, antialiased=False)
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+
+def PrintErrors(z_train,z_tilde,z_test,z_predict):
+	print("Training R2")
+	print(R2(z_train,z_tilde))
+	print("Training MSE")
+	print(MSE(z_train,z_tilde))
+	print("Test R2")
+	print(R2(z_test,z_predict))
+	print("Test MSE")
+	print(MSE(z_test,z_predict))
+	print("Variance for beta")
+	#print(BetaVar(X,alpha))
+
+def PlotErrors(ModelComplexity, training_data_error, test_data_error, error_type):
+    plt.plot(ModelComplexity,training_data_error,label=error_type+" train")
+    plt.plot(ModelComplexity,test_data_error,label=error_type+" test")
+    plt.xlabel("Polynomial degree")
+    plt.ylabel("Estimated error")
+    plt.legend()
+    plt.show()
